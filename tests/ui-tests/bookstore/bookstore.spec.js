f3b6b84abe0f2827e0d1aa7fdd4a7ddb9303fae9
Idan Charkachi <67859872+Charkech@users.noreply.github.com>
2022-01-12 12:40:42 +0200

    Update bookstore.spec.js

--- tests/ui-tests/bookstore/bookstore.spec.js
+++ tests/ui-tests/bookstore/bookstore.spec.js
@@ -4,5 +4,6 @@ const { test, expect } = require('@playwright/test');
 test('bookstore title test', async ({ page }) => {
   await page.goto('https://automationbookstore.dev/');
   const title = page.locator('.ui-header .ui-title');
+  console.log("hi");
   await expect(title).toHaveText('Automation Bookstore');
 });
