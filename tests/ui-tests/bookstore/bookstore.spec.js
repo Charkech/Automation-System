147733a72f828ffee055795e1f70ef6586f7c842
Charkech <idansoop@gmail.com>
2022-01-16 12:53:30 +0200

    removed note

--- tests/ui-tests/bookstore/bookstore.spec.js
+++ tests/ui-tests/bookstore/bookstore.spec.js
@@ -4,7 +4,6 @@ const { test, expect } = require('@playwright/test');
 test('BookStore-Title Test', async ({ page }) => {
   await page.goto('https://automationbookstore.dev/');
   const title = page.locator('.ui-header .ui-title');
-  console.log("hi");
   //We check if the title matches the expected value.
   await expect(title).toHaveText('Automation Bookstore');
 });
